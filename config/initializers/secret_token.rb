751b5edb72107f6437b419d88bd9d9567a3ef784
Patricia Navarro <patricianavarro@Patricia-Navarros-MacBook-Pro.local>
2012-06-08 15:43:54 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Restobot::Application.config.secret_token = '1a10980fe36f56553cf195899adf9f27337ccfd1a42411bc2628180d3f0073757b2b1dd26ce9d478f295dd5e2501397d043da5f7b15f664923d2b8d2a5b02714'
