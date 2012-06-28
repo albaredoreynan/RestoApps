887d34b6f1d26ea6048af3a7c4c2ee967cd572ce
Patricia Navarro <patricianavarro@Patricia-Navarros-MacBook-Pro.local>
2012-06-28 17:43:07 +0800

    changed association on server model to branch instead of concept

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,5 +1,5 @@
 class Server < ActiveRecord::Base
-  belongs_to :concept
+  belongs_to :branch
   # attr_accessible :title, :body
   has_many :sale_servers
   has_many :sales, :through => :sale_servers
