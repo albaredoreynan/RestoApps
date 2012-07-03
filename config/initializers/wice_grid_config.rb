a6009f3db4bf441bbb65a88f87d9a4efb9736540
Patricia Navarro <patricianavarro@Patricia-Navarros-MacBook-Pro.local>
2012-07-03 14:17:40 +0800

    fixed the calendar filter on wice grid

--- config/initializers/wice_grid_config.rb
+++ config/initializers/wice_grid_config.rb
@@ -131,11 +131,13 @@ if defined?(Wice::Defaults)
 
   # Format of the date displayed.
   # If you change the format, make sure to check if +DATE_PARSER+ can still parse this string.
-  Wice::Defaults::DATE_FORMAT     =  "%Y-%m-%d"
+  # Wice::Defaults::DATE_FORMAT     =  "%Y-%m-%d"
+  Wice::Defaults::DATE_FORMAT     =  "%Y-%d-%m"
 
   # Format of the date displayed in jQuery's Datepicker
   # If you change the format, make sure to check if +DATE_PARSER+ can still parse this string.
-  Wice::Defaults::DATE_FORMAT_JQUERY     =  "yy-mm-dd"
+  # Wice::Defaults::DATE_FORMAT_JQUERY     =  "yy-mm-dd"
+  Wice::Defaults::DATE_FORMAT_JQUERY     =  "yy-dd-mm"
 
 
   # With Calendar helpers enabled the parameter sent is the string displayed. This lambda will be given a date string in the
