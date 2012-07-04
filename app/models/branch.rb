0a8dfb248f2e5f5452b01af5e68fab20dbaf93e2
Patricia Navarro <patricianavarro@Patricia-Navarros-MacBook-Pro.local>
2012-07-04 15:30:50 +0800

    added endcount entries listing

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -2,9 +2,12 @@ class Branch < ActiveRecord::Base
   
   belongs_to :concept, :class_name => "Concept"
   belongs_to :client, :class_name => "Client"
+  
   has_many :roles, :dependent => :destroy
   
   has_many :suppliers, :dependent => :destroy
+  
+  has_many :endcounts, :dependent => :destroy
   
   attr_accessible :name, :concept_id, :client_id, :address, :contact_number
   
--- app/models/endcount.rb
+++ app/models/endcount.rb
@@ -1,6 +1,8 @@
 class Endcount < ActiveRecord::Base
   attr_accessible :entry_date, :branch_id, :item_counts_attributes
   
+  belongs_to :branch
+  
   has_many :item_counts
   has_many :items, :through => :item_counts
   
